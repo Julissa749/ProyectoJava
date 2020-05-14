d39336d5c8c0012a167753e2482365676bac386e
Julissa749 <salinasjulissa6@gmail.com>
2020-05-13 21:51:16 -0600

    mi segundo commit

--- src/javaProyect/pruebaproyect.java
+++ src/javaProyect/pruebaproyect.java
@@ -4,6 +4,7 @@ package javaProyect;
 public class pruebaproyect {
     public static void main(String[] args) {
         System.out.println("Hola Mundo ");
+        System.out.println("Hola Mundo 2");
     }
 }
  
